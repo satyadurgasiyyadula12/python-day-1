464519d689f3c1080a0ddd2fbabd7f4989785086
SIYYADULA SRI LAKSHMI SATYA DURGA <satya268844@gmail.com>
2025-06-22 18:29:15 +0530

    python operators

---
+++ 2.assignment operators.py
@@ -0,0 +1,28 @@
+
+"""
+AN OPEARATOR IS a special symbol which is used to perform an operator
+eg:c=a+b
+herec,a,b are operands
++ is a special symbol
+The python operators are:
+1.Arithmetic operator
+2.relational operator
+3.logical operator
+4.Bitwise operator
+5.Identity operator
+6.Membership operator
+7.Assignment operator
+"""
+
+
+"""
+An Arithmetic operator is used to perform some mathematical/arithmatic operations
+the operator are +, -, *, /, //,%,**
+Note;input()is afunction which takes input fron user
+"""
+
+a=int(input("enter a number:"))
+b=int(input('enter a number:'))
+print("the addition",a+b)
+print("the subtraction",a-b)
+print("the multiplication")
